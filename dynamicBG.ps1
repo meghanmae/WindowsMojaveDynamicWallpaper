c53b4baad6dde479e08b425a1389a6b1696ad620
Meghan <43832670+mmwoodfo@users.noreply.github.com>
2020-02-02 13:24:56 -0700

    Update dynamicBG.ps1

--- dynamicBG.ps1
+++ dynamicBG.ps1
@@ -1,13 +1,10 @@
 Function Set-WallPaper($value)
-
 {
-
  Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
 
- rundll32.exe user32.dll, UpdatePerUserSystemParameters
-
- Write $value
-
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters 
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters 
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters 
 }
 
 $Hour = Get-Date -Format HH
